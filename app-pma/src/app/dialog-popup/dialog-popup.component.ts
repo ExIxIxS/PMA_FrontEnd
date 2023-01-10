aa85c6d2fcc0723e3b84996a41e8cf3c3fc503bd
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-01-10 19:09:22 +0100

    feat: add popup dialog servise

---
+++ app-pma/src/app/dialog-popup/dialog-popup.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import { ErrorHandlerService } from '../errorHandler.service';
+import { ConfirmationService } from '../confirmation.service';
+
+
+
+@Component({
+  selector: 'app-dialog-popup',
+  templateUrl: './dialog-popup.component.html',
+  styleUrls: ['./dialog-popup.component.scss']
+})
+export class DialogPopupComponent {
+
+  constructor(
+    private errorHandlerService: ErrorHandlerService,
+    private confirmationService: ConfirmationService,
+    ) {
+  }
+
+  get type() {
+    return this.confirmationService.type;
+  }
+
+  get title() {
+    return this.confirmationService.title;
+  }
+
+  get isConfirmValid(): boolean {
+    return this.confirmationService.isConfirmValid;
+  }
+
+}
